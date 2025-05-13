43bd4e32d3c3fb29bb51db4fcdff86b76b9aca16
fchavonet <fchavonet@users.noreply.github.com>
2025-05-13 07:19:31 +0000

    Deploy personal website (v1.0) via GitHub Actions. 889bbd162a08b8ee93f6b4d807a262898ee0356d

---
+++ README.md
@@ -0,0 +1,125 @@
+<img height="50px" align="right" src="https://raw.githubusercontent.com/fchavonet/fchavonet/main/assets/images/logo-web.png" alt="Web logo">
+
+# Personal Website v1.0
+
+## 🔖 Table of contents
+
+<details>
+    <summary>
+        CLICK TO ENLARGE 😇
+    </summary>
+    📄 <a href="#description">Description</a>
+    <br>
+    🎓 <a href="#objectives">Objectives</a>
+    <br>
+    🔨 <a href="#tech-stack">Tech stack</a>
+    <br>
+    📂 <a href="#files-description">Files description</a>
+    <br>
+    💻 <a href="#installation_and_how_to_use">Installation and how to use</a>
+    <br>
+    🔧 <a href="#whats-next">What's next?</a>
+    <br>
+    ♥️ <a href="#thanks">Thanks</a>
+    <br>
+    👷 <a href="#authors">Authors</a>
+</details>
+
+## 📄 <span id="description">Description</span>
+
+This is the first version of my personal website, designed to showcase my professional skills and projects, and to provide a platform for potential employers or collaborators to get to know me better. While learning Bootstrap during my courses at Holberton School, I chose this technology to practice and develop my front-end skills. My objective was to quickly (one day) create a minimalistic and functional website, with a focus on a clean and responsive design that enhances user experience.
+
+That said, I am not fully satisfied with the current outcome, and a second version will be released soon. For now, this version simply fulfills the exercise requirements.
+
+## 🎓 <span id="objectives">Objectives</span>
+
+- Confirm my knowledge of the Bootstrap framework.
+- Practice JavaScript for user interaction.
+- Use libraries for efficiency.
+- Improve front-end development skills.
+- Enhance interactivity and UI/UX design.
+- Create a minimalist design.
+- Establish an online presence.
+
+## 🔨 <span id="tech-stack">Tech stack</span>
+
+<p align="left">
+    <img src="https://img.shields.io/badge/HTML5-e34f26?logo=html5&logoColor=white&style=for-the-badge" alt="HTML5 badge">
+    <img src="https://img.shields.io/badge/CSS3-1572b6?logo=css3&logoColor=white&style=for-the-badge" alt="CSS3 badge">
+    <img src="https://img.shields.io/badge/BOOTSTRAP-7952b3?logo=bootstrap&logoColor=white&style=for-the-badge" alt="Bootstrap badge">
+    <img src="https://img.shields.io/badge/JAVASCRIPT-f7df1e?logo=javascript&logoColor=black&style=for-the-badge" alt="JavaScript badge">
+    <img src="https://img.shields.io/badge/JSON-000000?logo=json&logoColor=white&style=for-the-badge" alt="JSON badge">
+    <img src="https://img.shields.io/badge/GIT-f05032?logo=git&logoColor=white&style=for-the-badge" alt="Git badge">
+    <img src="https://img.shields.io/badge/GITHUB-181717?logo=github&logoColor=white&style=for-the-badge" alt="GitHub badge">
+    <img src="https://img.shields.io/badge/MARKDOWN-000000?logo=markdown&logoColor=white&style=for-the-badge" alt="Markdown badge">
+    <img src="https://img.shields.io/badge/VS CODE-007acc?logo=data:image/svg+xml;base64,PCFET0NUWVBFIHN2ZyBQVUJMSUMgIi0vL1czQy8vRFREIFNWRyAxLjEvL0VOIiAiaHR0cDovL3d3dy53My5vcmcvR3JhcGhpY3MvU1ZHLzEuMS9EVEQvc3ZnMTEuZHRkIj4KDTwhLS0gVXBsb2FkZWQgdG86IFNWRyBSZXBvLCB3d3cuc3ZncmVwby5jb20sIFRyYW5zZm9ybWVkIGJ5OiBTVkcgUmVwbyBNaXhlciBUb29scyAtLT4KPHN2ZyBmaWxsPSIjZmZmZmZmIiB3aWR0aD0iODAwcHgiIGhlaWdodD0iODAwcHgiIHZpZXdCb3g9Ii0wLjUgMCAyNCAyNCIgeG1sbnM9Imh0dHA6Ly93d3cudzMub3JnLzIwMDAvc3ZnIj4KDTxnIGlkPSJTVkdSZXBvX2JnQ2FycmllciIgc3Ryb2tlLXdpZHRoPSIwIi8+Cg08ZyBpZD0iU1ZHUmVwb190cmFjZXJDYXJyaWVyIiBzdHJva2UtbGluZWNhcD0icm91bmQiIHN0cm9rZS1saW5lam9pbj0icm91bmQiLz4KDTxnIGlkPSJTVkdSZXBvX2ljb25DYXJyaWVyIj4KDTxwYXRoIGQ9Im0xNy44NTggMjMuOTk4LTkuNzcxLTkuNDg0LTUuODY2IDQuNDY1LTIuMjIxLTEuMTE1di0xMS43MTlsMi4yMzQtMS4xMjEgNS44NyA0LjQ2OSA5Ljc0Ny05LjQ5MyA1LjU4NyAyLjIzOXYxOS41MzFsLTUuNTc5IDIuMjN6bS0uNTYzLTE2LjE4Ni01LjU3NyA0LjE3MyA1LjU4IDQuMjAyem0tMTQuNTA3IDEuNjg1djUuMDE2bDIuNzg3LTIuNTI1eiIvPgoNPC9nPgoNPC9zdmc+&logoColor=white&style=for-the-badge" alt="VS Code badge">
+</p>
+
+Additional libraries and tools used:
+
+- [AOS (Animate On Scroll)](https://michalsnik.github.io/aos/): for scroll animations.
+- [EmailJS](https://www.emailjs.com): to handle the contact form submissions.
+- [Particles.js](https://vincentgarreau.com/particles.js/): for the background particle effect.
+
+## 📂 <span id="files-description">File description</span>
+
+| **FILE**      | **DESCRIPTION**                                     |
+| :-----------: | --------------------------------------------------- |
+| `assets`      | Contains the resources required for the repository. |
+| `index.html`  | Main HTML structure for the project.                |
+| `styles.css`  | Styles and animations for the project.              |
+| `script.js`   | Behavior script for interactivity.                  |
+| `CNAME`       | Defines the custom domain name for the website.     |
+| `README.md`   | The readme file you are currently reading 😉.       |
+
+## 💻 <span id="installation_and_how_to_use">Installation and how to use</span>
+
+**Installation:**
+
+1. Clone this repository:
+    - Open your preferred Terminal.
+    - Navigate to the directory where you want to clone the repository.
+    - Run the following command:
+
+```
+git clone https://github.com/fchavonet/web-personal_website.git
+```
+
+2. Open the repository you've just cloned.
+
+**How to use:**
+
+1. Open the `index.html` file in your web browser.
+
+You can also test the web application online by clicking [here](https://www.fchavonet.dev).
+
+<table>
+    <tr>
+        <th align="center" style="text-align: center;">Desktop view</th>
+        <th align="center" style="text-align: center;">Mobile view</th>
+    </tr>
+    <tr valign="top">
+        <td align="center">
+            <img src="./assets/images/screenshot_desktop.png">
+        </td>
+        <td align="center">
+            <img src="./assets/images/screenshot_mobile.png">
+        </td>
+    </tr>
+</table>
+
+## 🔧 <span id="whats-next">What's next?</span>
+
+- Enhance Accessibility.
+- SEO improvements.
+- Add more projects.
+- Create a better `v2.0`.
+
+## ♥️ <span id="thanks">Thanks</span>
+
+- A big thank you to my friends Pierre and Yoann, always available to test and provide feedback on my projects.
+
+## 👷 <span id="authors">Authors</span>
+
+**Fabien CHAVONET**
+- Github: [@fchavonet](https://github.com/fchavonet)
---
+++ script.js
@@ -0,0 +1,343 @@
+////////// SCROLL TO SECTIONS BEHAVIOR \\\\\\\\\\
+const navbarToggler = document.getElementById("navbar-toggler");
+const navbarMenu = document.getElementById("navbar-menu");
+const navLinks = document.querySelectorAll(".nav-link");
+
+navLinks.forEach(function (link) {
+  link.addEventListener("click", function (event) {
+    // Prevent the default behavior of the link.
+    event.preventDefault();
+
+    const sectionId = link.getAttribute("data-section-id");
+    const section = document.getElementById(sectionId);
+
+    if (section) {
+      if (sectionId === "projects") {
+        // If it's the "projects" section, apply the offset.
+        const sectionPosition = section.getBoundingClientRect().top + window.scrollY - 50;
+
+        // Scroll smoothly to the adjusted position.
+        window.scrollTo({ top: sectionPosition, behavior: "smooth" });
+      } else {
+        // Otherwise, use the standard scrollIntoView for smooth scrolling.
+        section.scrollIntoView({ behavior: "smooth" });
+      }
+
+      // Close the navbar menu if the screen width is less than 992px.
+      if (window.innerWidth < 992) {
+        navbarMenu.classList.remove("show");
+        navbarToggler.classList.add("collapsed");
+      }
+    } else {
+      // Log an error if the section ID is not found.
+      console.error("Section with ID " + sectionId + " not found.");
+    }
+  });
+});
+
+
+////////// ACTIVE LINK ON SCROLL BEHAVIOR \\\\\\\\\\
+document.addEventListener("DOMContentLoaded", function () {
+  const sections = document.querySelectorAll("section");
+  // const navLinks = document.querySelectorAll(".nav-link");
+
+  // Listen for the scroll event on the window.
+  window.addEventListener("scroll", function () {
+    let currentSection = "about";
+
+    // Loop through each section to check which is currently in view.
+    sections.forEach(function (section) {
+      const sectionTop = section.offsetTop;
+      const sectionHeight = section.clientHeight;
+
+      // Check if the section is within the current scroll position.
+      if (window.scrollY >= sectionTop - sectionHeight / 3) {
+        // Get the ID of the visible section.
+        currentSection = section.getAttribute("id");
+      }
+    });
+
+    // Update "active" class on nav links.
+    navLinks.forEach(function (link) {
+      // Remove "active" class from all links.
+      link.classList.remove("active");
+      if (link.getAttribute("data-section-id") === currentSection) {
+        // Add "active" class to the matching link.
+        link.classList.add("active");
+      }
+    });
+  });
+});
+
+
+////////// MOVE TO TOP BEHAVIOR \\\\\\\\\\
+const navbarBrand = document.querySelector(".navbar-brand");
+const moveToTopButton = document.getElementById("move-to-top-button");
+
+window.addEventListener("scroll", function () {
+  // Get the current scroll position.
+  const scrollPosition = window.scrollY || document.documentElement.scrollTop;
+
+  // Unhide the button if the user has scrolled more than 250 pixels.
+  if (scrollPosition > 250) {
+    moveToTopButton.classList.add("unhide");
+  } else {
+    moveToTopButton.classList.remove("unhide");
+  }
+});
+
+navbarBrand.addEventListener("click", function (event) {
+  // Prevent the default button behavior.
+  event.preventDefault();
+
+  window.scrollTo({ top: 0, behavior: "smooth" });
+});
+
+moveToTopButton.addEventListener("click", function (event) {
+  // Prevent the default button behavior.
+  event.preventDefault();
+
+  window.scrollTo({ top: 0, behavior: "smooth" });
+});
+
+
+////////// THEME TOGGLE BEHAVIOR \\\\\\\\\\
+const htmlPage = document.getElementById("html-page");
+
+const themeToggler = document.getElementById("theme-toggler");
+const themeIcon = themeToggler.querySelector("i");
+
+const bgTertiary = document.querySelectorAll(".bg-tertiary");
+
+// Get the current theme from localStorage or default to light.
+let currentTheme = localStorage.getItem("theme") || "light";
+
+// Update theme icon based on current theme.
+function updateThemeIcon() {
+  if (currentTheme === "light") {
+    themeIcon.classList.remove("bi-sun-fill");
+    themeIcon.classList.add("bi-moon-stars-fill");
+  } else {
+    themeIcon.classList.remove("bi-moon-stars-fill");
+    themeIcon.classList.add("bi-sun-fill");
+  }
+}
+
+// Update theme and localStorage.
+function updateTheme() {
+  htmlPage.setAttribute("data-bs-theme", currentTheme);
+
+  // Add or remove dark-mode class
+  if (currentTheme === "dark") {
+    bgTertiary.forEach(element => {
+      element.classList.add("dark-mode");
+    });
+  } else {
+    bgTertiary.forEach(element => {
+      element.classList.remove("dark-mode");
+    });
+  }
+
+  localStorage.setItem("theme", currentTheme);
+  updateThemeIcon();
+}
+
+// Toggle theme on button click
+themeToggler.addEventListener("click", function () {
+  if (currentTheme === "light") {
+    currentTheme = "dark";
+  } else {
+    currentTheme = "light";
+  }
+  updateTheme();
+});
+
+// Apply theme on page load.
+updateTheme();
+
+
+////////// POPHOVER BEHAVIOR \\\\\\\\\\
+function initializePopovers() {
+  const popoverTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="popover"]'));
+  const popoverList = popoverTriggerList.map(function (popoverTriggerEl) {
+    return new bootstrap.Popover(popoverTriggerEl);
+  });
+}
+
+document.addEventListener("DOMContentLoaded", function () {
+  initializePopovers();
+});
+
+
+////////// AUTO SECTION PADDING BEHAVIOR \\\\\\\\\\
+window.addEventListener("DOMContentLoaded", function () {
+  const navHeight = this.document.querySelector("nav").offsetHeight;
+  const sections = this.document.querySelectorAll("section");
+
+  //
+  sections.forEach(function (section) {
+    section.style.paddingTop = navHeight + "px";
+  });
+});
+
+
+////////// SKILLS CAROUSEL BEHAVIOR \\\\\\\\\\
+window.onload = function () {
+  // Selects the container of the skills carousel icons.
+  const skillsCarouselIconsContainer = document.getElementById("skills-carousel-icons-container");
+
+  // Clones the icons container and appends it for continuous scrolling.
+  const iconsContainerClone = skillsCarouselIconsContainer.cloneNode(true);
+  skillsCarouselIconsContainer.parentNode.appendChild(iconsContainerClone);
+
+  // Gets the total width of the skills carousel icons container
+  const iconsContainerWidth = skillsCarouselIconsContainer.scrollWidth;
+
+  let currentPosition = 0;
+  let scrolling = true;
+
+  function scrollCarousel() {
+    if (scrolling) {
+      currentPosition -= 1;
+    }
+
+    // Resets position if the container is fully scrolled.
+    if (Math.abs(currentPosition) >= iconsContainerWidth) {
+      currentPosition = 0;
+    }
+
+    skillsCarouselIconsContainer.style.transform = "translateX(" + currentPosition + "px)";
+    iconsContainerClone.style.transform = "translateX(" + (currentPosition + iconsContainerWidth) + "px)";
+
+    // Continues the animation on the next frame.
+    requestAnimationFrame(scrollCarousel);
+  }
+
+  scrollCarousel();
+
+  // Stop carousel scrolling on mouse over.
+  function stopScrolling() {
+    scrolling = false;
+  }
+  skillsCarouselIconsContainer.addEventListener("mouseover", stopScrolling);
+  iconsContainerClone.addEventListener("mouseover", stopScrolling);
+
+  // Restart carousel scrolling on mouse out.
+  function startScrolling() {
+    scrolling = true;
+  }
+  skillsCarouselIconsContainer.addEventListener("mouseout", startScrolling);
+  iconsContainerClone.addEventListener("mouseout", startScrolling);
+
+  initializePopovers();
+};
+
+
+////////// CONTACT FORM BEHAVIOR \\\\\\\\\\
+document.addEventListener("DOMContentLoaded", function () {
+  // Initialize EmailJS with public user ID.
+  emailjs.init("-nvDUA2GW_QoL_8nL");
+
+  // Selects the contact form.
+  const contactForm = document.getElementById("contact-form");
+
+  // Selects the name, email, and message input fields.
+  const name = document.getElementById("name");
+  const email = document.getElementById("email");
+  const message = document.getElementById("message");
+  const messageCharCount = document.getElementById("message-char-count");
+
+  // Selects the send button.
+  const sendButton = document.getElementById("send-button");
+
+  // Function to validate the email format.
+  function isValidEmail(email) {
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return emailRegex.test(email);
+  }
+
+  // Function to check if the form is completely filled out.
+  function checkFormCompletion() {
+    const isNameFilled = name.value.trim() !== "";
+    const isEmailFilled = email.value.trim() !== "";
+    const isMessageFilled = message.value.trim() !== "";
+    const isEmailValid = isValidEmail(email.value);
+
+    // Add or remove "invalid" class based on email validity.
+    if (!isEmailValid && isEmailFilled) {
+      email.classList.add("invalid");
+    } else {
+      email.classList.remove("invalid");
+    }
+
+    // Enable or disable send button.
+    if (isNameFilled && isEmailFilled && isEmailValid && isMessageFilled) {
+      sendButton.disabled = false;
+    } else {
+      sendButton.disabled = true;
+    }
+  }
+
+  // Update char count and check form.
+  contactForm.addEventListener("input", function () {
+    messageCharCount.textContent = message.value.length + "/500";
+    checkFormCompletion();
+  });
+
+  // Handles the form submission process.
+  contactForm.addEventListener("submit", function (event) {
+    // Prevent the default button behavior.
+    event.preventDefault();
+    sendButton.textContent = "Sending...";
+
+    emailjs.sendForm("service_37gv0v7", "template_lxmfrhn", this).then(function () {
+      alert("Your message has been sent!");
+      contactForm.reset();
+      sendButton.textContent = "Send Message";
+      sendButton.disabled = true;
+
+      // Reset message character count.
+      messageCharCount.textContent = "0/500";
+      // Remove "invalid" class from email input.
+      email.classList.remove("invalid");
+    }, function (error) {
+      alert("An error occurred, please try again.");
+      sendButton.textContent = "Send Message";
+    });
+  });
+});
+
+
+////////// ANIMATE ON SCROLL BEHAVIOR \\\\\\\\\\
+function initAOSForSmallScreens() {
+  if (window.innerWidth < 992) {
+    AOS.init({
+      // Enable AOS for small screens.
+      disable: false,
+      offset: 200,
+      delay: 0,
+      duration: 800,
+      easing: "ease",
+      once: false,
+      mirror: true,
+    });
+  } else {
+    AOS.init({
+      // Disable AOS for larger screens.
+      disable: true,
+    });
+  }
+}
+
+// Initialize on page load.
+initAOSForSmallScreens();
+
+// Re-initialize AOS when the window is resized.
+window.addEventListener("resize", function () {
+  initAOSForSmallScreens();
+});
+
+
+
+////////// BACKGROUND PARTICLES BEHAVIOR \\\\\\\\\\
+particlesJS.load("particles-js", "./assets/particles/particles.json");
